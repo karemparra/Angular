391c136c193d07c0f44c07b5b0275fceb5dba974
Karem Parra <karem.parra1998@gmail.com>
2021-12-06 12:02:04 -0500

    subiendo el archivo Usuario.ts

---
+++ src/app/model/Usuario.ts
@@ -0,0 +1,10 @@
+export class Usuario {
+    public idUsuario: number;
+    public nombre: string;
+    public edad: number;
+    public estado: boolean;
+
+    public Usuario(idUsuario:number, ){
+        this.idUsuario = idUsuario;
+    }
+}
